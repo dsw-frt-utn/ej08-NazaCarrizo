bba915d9033a7f0431fa790a6bea93230f51356c
Nazareno Carrizo <nazacarrizo18@gmail.com>
2026-05-02 00:26:20 -0300

    Problema 5 resuelto

---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public abstract class Sale
+    {
+        protected decimal _importe;
+
+        public decimal GetImporte()
+        {
+            return _importe;
+        }
+
+        public virtual decimal CalculateTotal()
+        {
+            return 0m;
+        }
+    }
+}
