899174c60e811d03b2a5536ef422b3d42d3e0389
Nazareno Carrizo <nazacarrizo18@gmail.com>
2026-05-02 01:46:04 -0300

    Problema 6 resuelto

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string cadena)
+        {
+            if (cadena == null)
+            {
+                return "SIN-CODIGO";
+            }
+            //Convertir la cadena a mayuscula
+            string codigo=cadena.ToUpper();
+            //Elimino el espacio en el inicio y al final si posee
+            codigo = codigo.Trim();
+            //reemplazo todo espacio restante con -
+            codigo=codigo.Replace(" ", "-");
+
+            return codigo;
+        }
+    }
+}
