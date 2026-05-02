cc7ac648714587a950d59d666e754d914aa80c6e
Nazareno Carrizo <nazacarrizo18@gmail.com>
2026-05-01 23:12:54 -0300

    Problema 4 resuelto

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema4
+    {
+
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma=0;
+            int cont = 0;
+            if (nota1.HasValue == true)
+            {
+                if (nota1 >= 0 && nota1 <= 10)
+                {
+                    suma = suma + (int)nota1;
+                    cont++;
+                }
+            }
+            if (nota2.HasValue == true)
+            {
+                if (nota2 >= 0 && nota2 <= 10)
+                {
+                    suma = suma + (int)nota2;
+                    cont++;
+                }
+            }
+            if (nota3.HasValue == true)
+            {
+                if (nota3 >= 0 && nota3 <= 10)
+                {
+                    suma = suma + (int)nota3;
+                    cont++;
+                }
+            }
+
+            if (cont != 0)
+            {
+                return (double)suma / cont;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
