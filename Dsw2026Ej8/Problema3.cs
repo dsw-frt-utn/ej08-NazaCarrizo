36549bb16fcb4a1442ff8d8881f98766dffb9b0e
Nazareno Carrizo <nazacarrizo18@gmail.com>
2026-05-01 23:36:55 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaEntero = originalValue;
+            copiaEntero++;
+            Product copiaProduct = product;
+            copiaProduct.ModificarDescripcion("NUEVA DESCRIPCION");
+            return $"{originalValue}-{copiaEntero}-{product.GetDescription()}";
+            //como copiaProduct y product hacen referencia al mismo objeto, da igual cual descripcion se imprima
+            //es la misma
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Product
+    {
+        private long _code;
+        private string _name;
+        private string _description;
+        private int _stock;
+
+        public Product() { 
+        }
+        public Product(long code, string name, string description, int stock)
+        {
+            _code = code;
+            _name = name;
+            _description = description;
+            _stock = stock;
+        }
+
+        public void ModificarDescripcion(string description)
+        {
+            _description= description;
+        }
+        public String GetDescription()
+        {
+            return _description;
+        }
+    }
+}
